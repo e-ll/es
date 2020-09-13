67040bca24b937690574ba6d8a8ad181a2a5794b
Ellpm <ellpm@ya.ru>
2020-09-13 21:02:14 +0300

    gitignore

--- config/keys_dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://localhost:27017/ecofest",
-  secretOrKey: "secret",
-  geocoderProvider: "yandex",
-  geocoderAPIKey: "ec5c9147-d4e0-4073-b78b-f588e9e38fda",
-  prodMongoUrl:
-    "mongodb+srv://user1:Mongo@cluster0.vp7ju.mongodb.net/ecofest?retryWrites=true&w=majority",
-};
