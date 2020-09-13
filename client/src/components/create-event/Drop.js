08007aabf11c3f78d018f1d05cd90bd4958e1a9d
Ellpm <ellpm@ya.ru>
2020-09-13 19:47:43 +0300

    fix

--- client/src/components/create-event/Drop.js
+++ client/src/components/create-event/Drop.js
@@ -20,7 +20,7 @@ export default class Example extends React.Component {
     this.componentConfig = {
       iconFiletypes: [".jpg", ".png", ".gif"],
       showFiletypeIcon: true,
-      postUrl: "http://127.0.0.1:8081/api/upload",
+      postUrl: "/api/upload",
     };
 
     // If you want to attach multiple callbacks, simply
