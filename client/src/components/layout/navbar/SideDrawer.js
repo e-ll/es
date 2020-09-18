dde496b1dbb6c8fa59176eb0eb6445eb83ff4d85
Ellpm <ellpm@ya.ru>
2020-09-18 15:13:39 +0300

    change menu

--- client/src/components/layout/navbar/SideDrawer.js
+++ client/src/components/layout/navbar/SideDrawer.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import { List, ListItem, ListItemText } from '@material-ui/core';
 
+import { List, ListItem, ListItemText, Link } from '@material-ui/core';
+import { Link as RouterLink } from "react-router-dom";
 import Logo from '../../../img/logo.png';
 
 const SideDrawer = ({ isAuthenticated, logout }) => {
     const authLinks = (
       <div>
         <ListItem button component={RouterLink} to="/create-event">
-          <ListItemText primary="Create Event" />
+          <ListItemText primary="Создать стенд" />
         </ListItem>
         <ListItem button component={RouterLink} to="/profile">
-          <ListItemText primary="Your Profile" />
+          <ListItemText primary="Мой профиль" />
         </ListItem>
         <ListItem button onClick={logout}>
-          <ListItemText primary="Logout" />
+          <ListItemText primary="Выйти" />
         </ListItem>
       </div>
     );
@@ -22,7 +22,7 @@ const SideDrawer = ({ isAuthenticated, logout }) => {
     const guestLinks = (
       <div>
         <ListItem button component={RouterLink} to="/login" color="textPrimary">
-          <ListItemText primary="Login" />
+          <ListItemText primary="Войти" />
         </ListItem>
         {/* <ListItem button component={RouterLink} to="/register" color="textPrimary">
           <ListItemText primary="Get Started" />
@@ -30,17 +30,20 @@ const SideDrawer = ({ isAuthenticated, logout }) => {
       </div>
     );
     
-    return(
+    return (
       <List component="nav">
-        <ListItem button component={RouterLink} to="/">
-          <img src={Logo} className="logo" alt="Logo" />
+        <ListItem button component={RouterLink} to="/events">
+          <ListItemText
+            style={{ color: "#D630B1", textAlign: "center" }}
+             primary="Festyline"
+          />
         </ListItem>
         <ListItem button component={RouterLink} to="/events">
-          <ListItemText primary="Events List" />
+          <ListItemText primary="Карта событий" />
         </ListItem>
         {isAuthenticated ? authLinks : guestLinks}
       </List>
-    )
+    );
 }
 
 export default SideDrawer;
