24ef832574cb2b96c723ecfe140e781217f806a9
Ellpm <ellpm@ya.ru>
2020-09-20 11:10:43 +0300

    serve

---
+++ client/src/components/firebase/con.js
@@ -0,0 +1,7 @@
+ const config = {
+   apiKey: "AIzaSyDYml5YkAaLQCjV_9nwGt3jMkyvBYpFdx8",
+   authDomain: "festyline.com",
+   databaseURL: "https://festyline.firebaseio.com",
+   storageBucket: "festyline.appspot.com",
+ };
+export default config
